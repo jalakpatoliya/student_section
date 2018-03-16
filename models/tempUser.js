d2e9d8c00f3d1a239c1895039c266d90e9554428
Manish |Leo <vinchidecode@gmail.com>
2018-03-16 14:54:05 +0530

    Delete tempUser.js

--- models/tempUser.js
+++
@@ -1,8 +0,0 @@
-var mongoose               = require("mongoose");
-
-var TempUserSchema = new mongoose.Schema({
-    username : String,
-    password : String
-})
-
-module.exports = mongoose.model("TempUser",TempUserSchema);
