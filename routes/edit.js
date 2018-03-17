1d00700c81b4b1639c632f03b98bb3380c79325d
Manish |Leo <vinchidecode@gmail.com>
2018-03-17 20:18:09 +0530

    Add files via upload

---
+++ routes/edit.js
@@ -0,0 +1,82 @@
+//=====================Importing all dependencies============
+var mongoose = require('mongoose'),
+    express  = require('express'),
+    mongoXlsx = require("mongo-xlsx"),
+    bodyParser = require("body-parser"),
+    multer    = require("multer"),
+    storage = multer.diskStorage(
+      {
+        destination: function (req, file, cb) {cb(null, 'uploads/')},
+        filename: function (req, file, cb) {cb(null, file.originalname)}
+      }),
+    authFunctions  = require('../validation/authFunctions');
+var upload = multer({ storage: storage }),
+    router      = express.Router();
+var flatten = require('flat');
+//===============================================================================
+//======================= Accuring internal module =============================
+//===============================================================================
+var Students   = require("../models/student");
+var model=null;
+var validate     = require('../validation/insertValidate');
+//==============================================
+//===================Using body parser==========
+//==============================================
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+//======================================
+// GET Route
+//======================================
+router.get("/edit",authFunctions.isLoggedIn,function(req,res){
+  var enroll = req.query.searched_enroll;
+  console.log(enroll);
+  Students.findById(enroll,function (err,data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("edit.ejs",{data:data});
+    }
+  })
+
+})
+//======================================
+// PoST Route
+//======================================
+router.post("/edit",authFunctions.isLoggedIn,function (req,res) {
+  console.log(req.body.basic);
+  console.log(req.body.enroll);
+  var obj = {basic:req.body.basic};
+console.log(obj);
+
+  Students.findByIdAndUpdate(req.body.enroll,flatten(obj),{overwrite:false},function (err,UpdatedData) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("UpdatedData:",UpdatedData);
+      Students.findById(req.body.enroll,function (err,data) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("show.ejs",{data:data});
+        }
+      })
+    }
+  })
+
+
+})
+
+//=========================================================
+//=============== isLoggedIn function======================
+//=========================================================
+//authenticating if user is loggedin or not
+function isLoggedIn(req,res,next) {
+  if (req.isAuthenticated()){
+    console.log("user is logged in");
+    return next();
+  }
+  console.log("user is not logged in");
+    res.redirect("/login");
+}
+module.exports = router;
---
+++ routes/search.js
@@ -0,0 +1,26 @@
+//=================================Importing all dependencies============
+var mongoose = require('mongoose'),
+    express  = require('express'),
+    router   = express.Router(),
+    Students  = require('../models/student'), //Importing multiple models and schemas
+    authFunctions = require('../validation/authFunctions');
+//=======================================================================
+
+//======================
+// Search ROUTE
+//======================
+router.get("/search",authFunctions.isLoggedIn,function(req,res){
+  var query = req.query.search_query;
+  Students.findById(query,function (err,data) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      res.render("show.ejs",{data:data});
+      // console.log(data);
+    }
+  })
+
+})
+
+module.exports = router;
