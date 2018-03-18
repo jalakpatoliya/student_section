200e049e1c2d4ccaf4a5fd1f5a0c43f40f6432cd
Manish |Leo <vinchidecode@gmail.com>
2018-03-18 22:48:34 +0530

    Add files via upload

--- routes/admin/notify.js
+++ routes/admin/notify.js
@@ -4,7 +4,9 @@ var mongoose = require('mongoose'),
     router   = express.Router(),
     Students  = require('../../models/student'); //Importing multiple models and schemas
     authFunctions = require('../../validation/authFunctions');
+
 /*******************************************************************************/
+    var schedule = require('node-schedule');
     var async = require("async");
     var nodemailer = require("nodemailer");
     var listofemails  = [];
@@ -28,6 +30,9 @@ var mongoose = require('mongoose'),
 // SHOW ROUTE
 //======================
 router.post("/notify",authFunctions.isLoggedIn,function(req,res){
+
+ if(req.body.eventoption=="instant"){
+
   ////////////////////////////////////////////////////////////////////////////////
   var to,sem,sub,content;
         sub=req.body.sub;
@@ -38,8 +43,8 @@ router.post("/notify",authFunctions.isLoggedIn,function(req,res){
        if(req.body.branch=="all"){
          console.log("all");
          if(sem=="all"){
-           data.forEach(elem=>{             
-               listofemails.push((elem.basic.email).trim());            
+           data.forEach(elem=>{
+               listofemails.push((elem.basic.email).trim());
            });//data.foreach
          }//all sem
          else{
@@ -152,9 +157,147 @@ router.post("/notify",authFunctions.isLoggedIn,function(req,res){
        //====================================================================
 
      });//.find
+}//fi eventoption==instant
+
+else if(req.body.eventoption=="timedriven"){
+console.log(req.body.eventoption);
+  var d =[],
+  d = (req.body.dt).split("-",3),
+  d1=(d[2].split("T",2))[0],
+  t =(((req.body.dt).split("-",3))[2].split("T",2))[1].split(":",2);
+  var date = new Date(d[0], d[1]-1, d1, t[0], t[1], 0);
+
+Students.find().lean().exec(function(err,data){
+  //////////////finding list of email//////////////////////////
+  listofemails=[]
+if(req.body.branch=="all"){
+ console.log("all");
+ if(sem=="all"){
+   data.forEach(elem=>{
+       listofemails.push((elem.basic.email).trim());
+   });//data.foreach
+ }//all sem
+ else{
+
+   if(sem=="first"){
+     data.forEach(elem=>{
+     if(elem.cur_sem==1||elem.cur_sem==2){
+       listofemails.push((elem.basic.email).trim());
+     }
+       });//data.forEach
+   }else if(sem=="second"){
+     data.forEach(elem=>{
+     if(elem.cur_sem==3||elem.cur_sem==4){
+       listofemails.push((elem.basic.email).trim());
+     }
+       });//data.forEach
+   }else if(sem=="third"){
+     data.forEach(elem=>{
+     if(elem.cur_sem==5||elem.cur_sem==6){
+       listofemails.push((elem.basic.email).trim());
+     }
+       });//data.forEach
+
+   }else if(sem=="fourth"){
+     data.forEach(elem=>{
+     if(elem.cur_sem==7||elem.cur_sem==8){
+       listofemails.push((elem.basic.email).trim());
+     }
+       });//data.forEach
+   }
+
+}//else
+}//if all
+else{
+ console.log("Specific");
+ sem = (req.body.year).trim();
+ if(sem=="all"){
+   data.forEach(elem=>{
+     if((elem.basic.branch).trim()==(req.body.branch).trim()){
+       listofemails.push((elem.basic.email).trim());
+     }
+   });//data.foreach
+ }//all sem
+ else{
+   data.forEach(elem=>{
+     if((elem.basic.branch).trim()==(req.body.branch).trim()){
+   if(sem=="first"){
+     if(elem.cur_sem==1||elem.cur_sem==2){
+       listofemails.push((elem.basic.email).trim());
+     }
+
+   }else if(sem=="second"){
+     if(elem.cur_sem==3||elem.cur_sem==4){
+       listofemails.push((elem.basic.email).trim());
+     }
+
+   }else if(sem=="third"){
+     if(elem.cur_sem==5||elem.cur_sem==6){
+       listofemails.push((elem.basic.email).trim());
+     }
+
+   }else if(sem=="fourth"){
+     if(elem.cur_sem==7||elem.cur_sem==8){
+       listofemails.push((elem.basic.email).trim());
+     }
+   }
+ }//Branch
+});//data.foreach
+ }//else all sem
+}//else specific
+  //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  ///////preparing job schedule//////////////////////////////
+   schedule.scheduleJob(date, function(){
+    ////////////////preparing mail event///////
+    var to,sem,sub,content;
+          sub=req.body.sub;
+          content=req.body.msg;
+    function massMailer() {
+      var self = this;
+   //   Fetch all the emails from database and push it in listofemails process remaining
+  self.invokeOperation();
+ };
+
+ massMailer.prototype.invokeOperation = function() {
+  var self = this;
+  async.each(listofemails,self.SendEmail);
+ }
+
+ massMailer.prototype.SendEmail = function(Email,callback) {
+  console.log("Sending email to " + Email);
+  var self = this;
+  self.status = false;
+  async.waterfall([
+    function(callback) {
+       var mailOptions = {
+           from: 'GEC Modasa <fygecmodasa@gmail.com>',
+           to: Email,
+           subject: sub,
+           text: content
+       }
+      transporter.sendMail(mailOptions, function(error, info) {
+        if(error) {
+          console.log(error)
+          failure_email.push(Email);
+        } else {
+          self.status = true;
+          success_email.push(Email);
+        }
+      });
+    },
+  ]);
+ }
+
+ new massMailer(); //lets begin
+    ///////////////////////////////////////////
+  });//schedule.job
+  //////////////////////////////////////////////////////////
+});//students.find
 
 
- console.log(listofemails,"--",sem);
+
+}//fi eventoption==timedriven
+
 
 
        //===============================================================
@@ -164,6 +307,9 @@ router.post("/notify",authFunctions.isLoggedIn,function(req,res){
   res.render("./admin/notify.ejs")
 
 })
+//=============================================================================
+//                       GET route
+//=============================================================================
 router.get("/notify",authFunctions.isLoggedIn,function(req,res){
   res.render("./admin/notify.ejs")
 })
