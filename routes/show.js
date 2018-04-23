721708738b144379dcf39d22c45c951156e42e00
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-04-23 10:45:30 +0530

    working on fees receipts module

--- routes/show.js
+++ routes/show.js
@@ -6,24 +6,6 @@ var mongoose = require('mongoose'),
     authFunctions = require('../validation/authFunctions'),
     flatten       = require('flat');
 //=======================================================================
-//==============================================================================
-//==================== defining sync.do ========================================
-//=============================================================================
-var sync = {
-    _deasync: require('deasync'),
-    _done: false,
-    do: function(callback, sleep) {
-        this._done = false;
-        callback();
-        while ( ! this._done) {
-            this._deasync.sleep(sleep ? sleep : 100);
-        }
-    },
-    done: function() {
-        this._done = true;
-    }
-}
-
 
 //======================
 // SHOW ROUTE
@@ -50,31 +32,26 @@ router.post("/showReceiptsForm",authFunctions.isLoggedIn,function(req,res){
 
 
   if (req.body.ReceiptType=='EFee_pdf') {
-    sync.do(function(){
-    var obj = { cur_sem:req.body.sem,basic:{branch:req.body.branch}};
-    var had;var had_not=[];
-    console.log(flatten(obj));
 
+    var obj = { cur_sem:req.body.sem,basic:{branch:req.body.branch}};
+    console.log(flatten(obj));
     Students.find().and([{$or:[{EFee_pdf:false},{EFee_pdf:null}]},flatten(obj)]).exec(function(err,studs_not){
       // console.log("studs_not:",studs_not);
-      had_not=studs_not;
-      console.log("Inside had_not:",had_not);
+      Students.find().and([{EFee_pdf:true},flatten(obj)]).exec(function(err,studs_had){
+        // console.log("studs_had:",studs_had);
+        res.render("./user/showReceipts.ejs",{studs_not:studs_not,studs_had:studs_had ,EFee:true})
+      })
     })
 
-    Students.find().and([{EFee_pdf:true},flatten(obj)]).exec(function(err,studs_had){
-      // console.log("studs_had:",studs_had);
-      had=studs_had;
-    })
-    console.log("Outside had:",had);
-    console.log("Outside had_not:",had_not);
-    res.render("./user/showReceipts.ejs",{studs_not:had_not,sutds_had:had,EFee:true})
-    })
   } else if (req.body.ReceiptType=='TFee_pdf') {
     var obj = { cur_sem:req.body.sem,basic:{branch:req.body.branch}};
     console.log(flatten(obj));
     Students.find().and([{$or:[{TFee_pdf:false},{TFee_pdf:null}]},flatten(obj)]).exec(function(err,studs_not){
       console.log(studs_not);
-      res.render("./user/showReceipts.ejs",{studs_not:studs_not,EFee:false})
+      Students.find().and([{TFee_pdf:true},flatten(obj)]).exec(function(err,studs_had){
+        // console.log("studs_had:",studs_had);
+        res.render("./user/showReceipts.ejs",{studs_not:studs_not,studs_had:studs_had,EFee:false})
+      })
     })
   }
 
